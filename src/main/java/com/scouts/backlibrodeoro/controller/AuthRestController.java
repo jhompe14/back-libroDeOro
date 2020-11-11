d84ec7b8970ecfb16b9401b55ed7d4a4a9cc1ddd
jhompe <jhonedisonrivera@gmail.com>
2020-11-10 21:52:42 -0500

    Creacion del ping de la aplicacion

--- src/main/java/com/scouts/backlibrodeoro/controller/AuthRestController.java
+++ src/main/java/com/scouts/backlibrodeoro/controller/AuthRestController.java
@@ -3,6 +3,7 @@ package com.scouts.backlibrodeoro.controller;
 import com.scouts.backlibrodeoro.auth.JWTGenerator;
 import com.scouts.backlibrodeoro.dto.request.AuthRequestDTO;
 import com.scouts.backlibrodeoro.exception.NegocioException;
+import com.scouts.backlibrodeoro.model.Grupo;
 import com.scouts.backlibrodeoro.service.UsuarioService;
 import com.scouts.backlibrodeoro.types.TypeException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*", methods= {RequestMethod.POST})
+@CrossOrigin(origins = "*", methods= {RequestMethod.POST, RequestMethod.GET})
 public class AuthRestController {
 
     private final UsuarioService usuarioService;
@@ -36,6 +39,11 @@ public class AuthRestController {
         }catch (Exception ex){
             return new ResponseEntity(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> pingAuth()  {
+        return new ResponseEntity("OK", HttpStatus.OK);
     }
 
 }
