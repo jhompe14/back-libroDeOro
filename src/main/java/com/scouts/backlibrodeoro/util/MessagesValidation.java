6a5c72fde1d180aff2b3b1fa97b345f460c2bce8
jhompe <jhonedisonrivera@gmail.com>
2021-01-12 20:42:49 -0500

    Correccion ortografica

--- src/main/java/com/scouts/backlibrodeoro/util/MessagesValidation.java
+++ src/main/java/com/scouts/backlibrodeoro/util/MessagesValidation.java
@@ -4,10 +4,10 @@ public class MessagesValidation {
 
     public static final String ERROR_GRUPO_NO_EXISTE = "El grupo no existe";
     public static final String ERROR_RAMA_NO_EXISTE = "La rama no existe";
-    public static final String ERROR_SECCION_NO_EXISTE = "La seccion no existe";
+    public static final String ERROR_SECCION_NO_EXISTE = "La secci\u00F3n no existe";
     public static final String ERROR_CARGO_NO_EXISTE = "El cargo no existe";
     public static final String ERROR_USUARIO_NO_EXISTE = "El usuario no existe";
-    public static final String ERROR_ANECDOTA_NO_EXISTE = "La anecdota no existe";
+    public static final String ERROR_ANECDOTA_NO_EXISTE = "La an\u00E9cdota no existe";
 
     public static final String ERROR_CODIGO_RECOVERED_CONTRASENA_NO_VALIDO = "El c\u00F3digo de recuperaci\u00F3n no esta disponible";
     public static final String ERROR_CORREO_RECEPTOR_NO_VALIDO = "El correo del receptor no es valido";
@@ -15,22 +15,22 @@ public class MessagesValidation {
     public static final String VALIDATION_NOMBRE_OBLIGATORIO = "El nombre es obligatorio";
     public static final String VALIDATION_TODOS_CAMPOS_OBLIGATORIOS = "Debe diligenciar todos los campos obligatorios: ";
     public static final String VALIDATION_EDAD_MINIMA_MAXIMA =
-            "Revise la edad maxima y minima no debe ser un numero menor a 0 ni superar los 150";
+            "Revise la edad m\u00E1xima y m\u00EDnima no debe ser un numero menor a 0 ni superar los 150";
     public static final String VALIDATION_EDAD_MAXIMA_MAJOR_EDAD_MINIMA =
-            "La edad minima no puede ser mayor a la edad maxima";
+            "La edad m\u00EDnima no puede ser mayor a la edad m\u00E1xima";
 
     public static final String VALIDATION_GRUPO_RAMAS_ACTIVAS = "El grupo posee ramas activas";
     public static final String VALIDATION_GRUPO_CARGOS_ACTIVOS = "El grupo posee cargos activos";
 
     public static final String VALIDATION_RAMA_SECCIONES_ACTIVAS = "La rama posee secciones activas";
     public static final String VALIDATION_RAMA_CARGOS_ACTIVOS = "La rama posee cargos activos";
-    public static final String VALIDATION_RAMA_EDAD_MINIMA = "La edad minima es obligatoria";
-    public static final String VALIDATION_RAMA_EDAD_MAXIMA = "La edad maxima es obligatoria";
+    public static final String VALIDATION_RAMA_EDAD_MINIMA = "La edad m\u00EDnima es obligatoria";
+    public static final String VALIDATION_RAMA_EDAD_MAXIMA = "La edad m\u00E1xima es obligatoria";
 
 
-    public static final String VALIDATION_SECCION_CARGOS_ACTIVOS = "La seccion posee cargos activos";
+    public static final String VALIDATION_SECCION_CARGOS_ACTIVOS = "La secci\u00F3n posee cargos activos";
 
-    public static final String NOT_FOUND_USUARIO_CONTRASENA = "Usuario y contrasena incorrectos";
+    public static final String NOT_FOUND_USUARIO_CONTRASENA = "Usuario y contrase\u00F1a incorrectos";
     public static final String DILIGENCIAR_USUARIO_CONTRASENA =
             "Debe diligenciar correctamente usuario y contrase\u00F1a";
 
@@ -45,8 +45,8 @@ public class MessagesValidation {
     public static final String VALIDATION_TIPO_USUARIO_REQUIRED = "El tipo de usuario debe ser uno valido";
     public static final String VALIDATION_CORREO_REQUIRED = "El tipo de correo debe ser uno valido";
     public static final String VALIDATION_TELEFONO_REQUIRED =
-                                "El telefono es obligatorio y ademas debe tener entre 7 y 12 numeros";
-    public static final String VALIDATION_DIRECCION_REQUIRED = "La direccion es obligatoria";
+                                "El tel\u00E9fono es obligatorio y adem\u00E1s debe tener entre 7 y 12 n\u00FAmeros";
+    public static final String VALIDATION_DIRECCION_REQUIRED = "La direcci\u00F3n es obligatoria";
     public static final String VALIDATION_CIUDAD_REQUIRED = "La ciudad es obligatoria";
 
     public static final String VALIDATION_TRAYECTORIA_REQUIRED =
@@ -58,10 +58,10 @@ public class MessagesValidation {
 
     public static final String VALIDATION_ANECDOTA_DESCRIPCION = "La descripci\u00F3n es obligatoria";
     public static final String VALIDATION_ANECDOTA_FECHA =
-            "La fecha de la anecdota no puede ser superior a la fecha actual";
+            "La fecha de la an\u00E9cdota no puede ser superior a la fecha actual";
     public static final String VALIDATION_ANECDOTA_CATALOG_DATES = "La fecha inicio no puede ser superior a la fecha final";
-    public static final String VALIDATION_TIPO_ESTADO_ANECDOTA = "Este estado de anecdota no es valido";
-    public static final String VALIDATION_RAMA_ANECDOTA = "La anecdota debe estar asociada a una rama o seccion";
+    public static final String VALIDATION_TIPO_ESTADO_ANECDOTA = "Este estado de an\u00E9cdota no es valido";
+    public static final String VALIDATION_RAMA_ANECDOTA = "La an\u00E9cdota debe estar asociada a una rama o secci\u00F3n";
     public static final String VALIDATION_ATTACHED_ANECDOTA = "El archivo adjunto no existe";
     public static final String VALIDATION_ATTACHED_FORMAT_ANECDOTA = "El formato de los adjuntos debe ser .PNG .JPG .JPEG";
     public static final String VALIDATION_VIDEO_FORMAT_ANECDOTA = "El nombre y la url son obligatorios para adjuntar videos";
@@ -72,7 +72,7 @@ public class MessagesValidation {
     public static final String VALIDATION_USUARIO_MODIFICACION_ANECDOTA =
             "Si el estado es pendiente modificaci\u00F3n debe agregar un usuario";
 
-    public static final String VALIDATION_TYPE_CHANGE_CONTRASENA = "Este tipo de cambio de contrasena no es valido";
+    public static final String VALIDATION_TYPE_CHANGE_CONTRASENA = "Este tipo de cambio de contrase\u00F1a no es valido";
     public static final String VALIDATION_TYPE_CHANGE_CONTRASENA_MODIFICATION_REQUIRED =
             "Los campos obligatorios son </br> Contrase\u00F1a Actual </br> Nueva Contrase\u00F1a </br> Confirmar Contrase\u00F1a ";
     public static final String VALIDATION_TYPE_CHANGE_CONTRASENA_RECOVERED_REQUIRED =
